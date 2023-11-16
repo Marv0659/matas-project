d7e1f20239c0526494a2f9a7e57395058113890b
marv0659 <marv0659@stud.kea.dk>
2023-11-16 19:36:52 +0100

    Data cleaned og formateret (mangler stadig)

---
+++ index.js
@@ -0,0 +1,487 @@
+const raw = `Hudtype	Normal
+Hudtype	Tør
+Hudtype	Sensitiv
+Hudtype	Kombineret
+Form	Creme
+Form	Gel
+Form	Farvet dagcreme
+Specialbehov	Pigmentforandringer
+Specialbehov	Anti Age
+Specialbehov	Uren hud
+Indhold	Uden parfume
+Indhold	Uden parabener
+Indhold	Med økologisk indhold
+Solfaktor	Med solfaktor
+Solfaktor	Lav 6-10
+Solfaktor	Mellem 15-25
+Solfaktor	Høj 30-50
+Certificeringer	Astma Allergi Danmark (Den Blå Krans)
+Certificeringer	Ecocert
+Certificeringer	Svanemærket
+Certificeringer	NATRUE (Natur-certificeret)
+Certificeringer	Vegan Trademark
+Certificeringer	Allergy Certified
+Form	Lotion
+Form	Olie
+Form	Serum
+Form	Mousse
+Anvendelse	Børn
+Anvendelse	Ansigt
+Anvendelse	Krop
+Anvendelse	Læber
+Anvendelse	Vinter
+Vandafvisende	Vandfast
+Vandafvisende	Meget vandfast
+Certificeringer	BDIH (Natur-certificeret)
+Certificeringer	Blomsten
+Specialbehov	Rødme
+Form	Mineral
+Form	Flydende
+Form	Kompakt
+Form	Cushion
+Form	Stift
+Form	Mousse
+Dækkeevne	Let
+Dækkeevne	Medium
+Dækkeevne	Fuld
+Finish	Mat
+Finish	Naturlig
+Finish	Glødende
+Finish	Mat
+Finish	Neutral
+Finish	Shine
+Finish	Mat
+Finish	Glitter
+Finish	Satin
+Finish	Metallic
+Finish	Glossy
+Specialfarver	Bronze
+Specialfarver	Gennemsigtig
+Specialfarver	Guld
+Specialfarver	Sølv
+Køn	Kvinde
+Køn	Mand
+Indhold	Vegansk
+Køn	Unisex
+Køn	Barn
+Certificeringer	RSPO (bæredygtig palmeolie)
+Duftfamilie	Blomster
+Duftfamilie	Orientalsk og krydret
+Duftfamilie	Frisk og ren
+Duftfamilie	Træ og læder
+Duftfamilie	Sød og varm
+Indhold	Sojabaseret
+Størrelse	Under 30 ml
+Størrelse	30-50 ml
+Størrelse	50-100 ml
+Form	Spray
+Form	Stick
+Form	Roll-on
+Form	Spray
+Form	Rollerball
+Certificeringer	Cruelty Free
+Certificeringer	Ø-mærket
+Certificeringer	Fairtrade
+Certificeringer	Halal
+Certificeringer	Oeko-tex
+Modtager	Til hende
+Modtager	Til ham
+Modtager	Til teenageren
+Modtager	Til mor
+Modtager	Til far
+Modtager	Til den gravide
+Modtager	Til værtinden
+Highlights	Kun i Matas
+Highlights	Limited Edition
+Highlights	Online Only
+Underkategori	Dufte
+Underkategori	Hudpleje
+Underkategori	Makeup
+Underkategori	Hårpleje
+Underkategori	Helse
+Indhold	Reef Safe
+Indhold	Uden acetone
+Hudtype	Fedtet
+Størrelse	Gaveæske
+Størrelse	Rejsestørrelse
+Størrelse	Over 100 ml
+Størrelse	Rejsestørrelse
+Størrelse	Gaveæske
+Form	Olie
+Form	Kompakt
+Form	Creme
+Specialbehov	Porer
+Specialbehov	Ujævn hudtone
+Specialbehov	T-zone
+Egenskaber	Giver glød
+Egenskaber	Fugtgivende
+Egenskaber	Beskyttende og forebyggende
+Egenskaber	Detox-effekt
+Egenskaber	Reparerende
+Egenskaber	Eksfolierende
+Egenskaber	Matterende (Anti-shine)
+Egenskaber	Beroligende og lindrende
+Alder	Teenager
+Alder	20'erne
+Alder	30'erne
+Alder	40'erne
+Alder	50+
+Form	Mælk
+Form	Stift
+Form	Maske
+Form	Balm
+Specialbehov	Mørke rande
+Form	Farvet læbepleje
+Form	Scrub
+Egenskaber	Dybderensende
+Form	Ler
+Form	Pads
+Form	Salt
+Hudtone	Lys
+Hudtone	Medium
+Hudtone	Dyb
+Undertone	Kold (Blålige blodårer)
+Undertone	Neutral (Blå-grønne blodårer)
+Undertone	Varm (Grønlige blodårer)
+Finish	Shine
+Finish	Glansfuld
+Finish	Perlemor
+Certificeringer	ECARF
+Egenskaber	Vandfast
+Egenskaber	Fugtgivende
+Egenskaber	Langtidsholdbar
+Egenskaber	Olieabsorberende
+Egenskaber	Plejende
+Egenskaber	Hurtigttørrende
+Egenskaber	Vækstfremmende
+Egenskaber	Styrkende og hærdende
+Egenskaber	Udglatter riller
+Egenskaber	Renser og affedter
+Farveintensitet	Lav
+Farveintensitet	Mellem
+Farveintensitet	Høj
+Størrelse	Single
+Størrelse	Palette
+Form	Løst pudder
+Form	Creme
+Form	Gel
+Form	Olie
+Form	Spray
+Form	Balm
+Form	Lak
+Form	Serum
+Form	Kohl
+Form	Blyant
+Form	Pomade
+Form	Pudder
+Form	Farvet læbepomade
+Egenskaber	Plumb-effekt
+Egenskaber	Volumen
+Egenskaber	Længde
+Egenskaber	Curl
+Egenskaber	Fiber
+Egenskaber	Ikke vandfast
+Egenskaber	Former
+Egenskaber	Fiksering
+Form	Svamp
+Sæson	Forår
+Sæson	Sommer
+Sæson	Efterår
+Sæson	Vinter
+Sæson	Dag
+Sæson	Nat
+Duftfamilie	Aromatisk og urteagtig
+Form	Stick
+Form	Roll-on
+Form	Creme
+Hårtype	Krøllet hår
+Hårtype	Fedtet hår
+Hårtype	Tørt hår
+Form	Spray
+Form	Servietter
+Form	Pudder
+Form	Salve
+Form	Fast
+Form	Flydende
+Hårtype	Fint hår
+Hårtype	Farvet hår
+Hårtype	Gråt hår
+Hårtype	Kruset hår
+Hårtype	Skadet hår
+Hårtype	Tykt hår
+Hårtype	Normalt hår
+Hårtype	Blondt hår
+Hårtype	Afbleget hår
+Specialbehov	Skæl
+Specialbehov	Tør og irriteret hovedbund
+Specialbehov	Hårtab
+Specialbehov	Fedtet hovedbund
+Specialbehov	Filtret hår
+Specialbehov	Spaltede spidser
+Specialbehov	Sensitiv hovedbund
+Egenskaber	Heat Protection
+Egenskaber	Anti Frizz
+Egenskaber	Dybderensende
+Egenskaber	Anti Yellow
+Egenskaber	Med sol- og UV-beskyttelse
+Egenskaber	Giver glans
+Egenskaber	Definerer krøller
+Egenskaber	Blødgørende
+Egenskaber	Fugtgivende
+Egenskaber	Styrkende og forebyggende
+Egenskaber	Farvebevarende
+Egenskaber	Reparerende
+Egenskaber	Dækker grå hår
+Tonehøjde	1 Sort
+Tonehøjde	2 Brunsort
+Tonehøjde	3 Mørkebrun
+Tonehøjde	4 Brun
+Tonehøjde	5 Lysebrun
+Tonehøjde	6 Mørkeblond
+Tonehøjde	7 Mellemblond
+Tonehøjde	8 Lysblond
+Tonehøjde	9 Meget lysblond
+Tonehøjde	10 Helt lysblond
+Primær refleks	0 Ingen
+Primær refleks	1 Blålig (ask)
+Primær refleks	2 Violet (iriserende)
+Primær refleks	3 Gullig (gylden)
+Primær refleks	4 Orange (kobber)
+Primær refleks	5 Blålilla (mahogni)
+Primær refleks	6 Rødlig
+Primær refleks	7 Grønlig (metallisk)
+Primær refleks	8 Rødbrun (mocca)
+Sekundær refleks	0 Ingen
+Sekundær refleks	1 Blålig (ask)
+Sekundær refleks	2 Violet (iriserende)
+Sekundær refleks	3 Gullig (gylden)
+Sekundær refleks	4 Orange (kobber)
+Sekundær refleks	5 Blålilla (mahogni)
+Sekundær refleks	6 Rødlig
+Sekundær refleks	7 Grønlig (metallisk)
+Sekundær refleks	8 Rødbrun (mocca)
+Farve	Blond
+Farve	Blå
+Farve	Brun
+Farve	Pink
+Farve	Grå
+Farve	Rød
+Farve	Lilla og violet
+Farve	Sort
+Farve	Grøn
+Farve	Lyserød
+Farve	Kobber
+Farve	Lysebrun
+Farve	Orange
+Farve	Mørkebrun
+Farve	Askeblond
+Farve	Ombre
+Farve	Silver (sølv)
+Farve	Platinblond
+Farve	Lysblond
+Farve	Mørkeblond
+Farve	Rødbrun
+Farve	Glitter
+Farve	Andre farver
+Hårlængde	Kort
+Hårlængde	Mellem (skulderlangt)
+Hårlængde	Langt
+Hold	Let
+Hold	Medium
+Hold	Stærkt
+Form	Flydende
+Form	Fast
+Form	Spray
+Form	Creme
+Form	Olie
+Form	Maske
+Form	Serum
+Form	Mælk
+Form	Lotion
+Form	Voks
+Form	Pomade
+Form	Gel
+Form	Mousse
+Form	Pudder
+Form	Scrub
+Egenskaber	Trådløs
+Egenskaber	Genopladelig
+Egenskaber	Ergonomisk greb
+Egenskaber	Quick Charge-funktion
+Egenskaber	LED-display
+Egenskaber	Med hukommelsesfunktion
+Egenskaber	Ionisk
+Egenskaber	Automatisk sluk-funktion
+Egenskaber	Keramisk belægning
+Egenskaber	Temperaturlås
+Egenskaber	Koldluftfunktion
+Egenskaber	Giver volumen
+Egenskaber	Antistatisk
+Specialbehov	Poser under øjnene
+Indhold	Curly Girl-venlig
+Indhold	Økologisk
+Indhold	Uden olie
+Specialbehov	Meget tør hud og kløe
+Egenskaber	Kølende
+Egenskaber	Antiperspirant
+Form	Drops
+Certificeringer	EU's økologimærke
+Certificeringer	Nøglehulsmærket
+Certificeringer	Rainforest Alliance
+Certificeringer	Det grønne hjerte
+Form	Pads
+Form	Servietter
+Specialbehov	Appelsinhud
+Specialbehov	Strækmærker
+Specialbehov	Hård hud
+Specialbehov	Hælrevner
+Specialbehov	Hævelse
+Indhold	Glutenfri
+Indhold	Laktosefri
+Indhold	Uden sukker
+Indhold	Med sødemiddel
+Alder	Børn 0-3 år
+Alder	Børn 4-10 år
+Alder	Voksne (fra 11 år)
+Alder	Voksne 50+ år
+Alder	Gravid/ ammende
+Størrelse	XXS
+Størrelse	XS
+Størrelse	S
+Størrelse	M
+Størrelse	L
+Størrelse	XL
+Størrelse	XXL
+Størrelse	XXXL
+Størrelse	XXXXL
+Størrelse	20
+Størrelse	21
+Størrelse	22
+Størrelse	23
+Størrelse	24
+Størrelse	25
+Størrelse	26
+Størrelse	27
+Størrelse	28
+Størrelse	29
+Størrelse	30
+Størrelse	31
+Størrelse	32
+Størrelse	33
+Størrelse	34
+Størrelse	35
+Størrelse	36
+Størrelse	37
+Størrelse	38
+Størrelse	39
+Størrelse	40
+Størrelse	41
+Størrelse	42
+Størrelse	43
+Størrelse	44
+Størrelse	45
+Størrelse	46
+Størrelse	47
+Størrelse	48
+Størrelse	49
+Størrelse	50
+Tåler tørretumbler	Tåler tørretumbler
+Tåler tørretumbler	Tåler ikke tørretumbler
+Vaskeanvisning	Vaskes på max 30⁰C
+Vaskeanvisning	Vaskes på max 40⁰C
+Vaskeanvisning	Vaskes på max 60⁰C
+Vaskeanvisning	Kun håndvask
+Vaskeanvisning	Vaskes på max 90⁰C
+Nichedufte	Nicheduft
+Dermatologisk hudpleje	Dermatologisk hudpleje
+Kendt fra frisøren	Kendt fra frisøren
+Form	Pulver
+Form	Kapsel
+Form	Næsespray
+Form	Næsedråber
+Form	Tablet
+Form	Øjendråber
+Form	Gummies
+Form	Brusetablet
+Form	Tyggetablet
+Form	Flydende
+Form	Stikpille
+Form	Sugetablet
+Smag	Vanilje
+Smag	Chokolade
+Smag	Neutral
+Smag	Frugt & bær
+Smag	Cola
+Smag	Nødder
+Smag	Nougat
+Smag	Cookie dough
+Smag	Marcipan
+Smag	Mint
+Smag	Kanel
+Smag	Citrus
+Smag	Karamel
+Smag	Kaffe
+Batteritype	AA
+Batteritype	AAA
+Batteritype	Litiumbatteri
+Sportsgren	Styrketræning
+Sportsgren	Løb
+Sportsgren	Yoga
+Sportsgren	Pilates
+Sportsgren	Gymnastik
+Sportsgren	Afspænding/massage
+Sportsgren	Crossfit
+Sportsgren	Skiløb
+Sportsgren	Konditionstræning
+Sportsgren	Boksning
+Sportsgren	Cykling
+Alder	3 mdr.
+Alder	6 mdr.
+Alder	9 mdr.
+Alder	12 mdr.
+Alder	2 år
+Alder	3 år
+Alder	18 mdr.
+Form	Rund
+Form	Flad (symmetrisk)
+Form	Anatomisk
+Proteinkilde	Hvede
+Proteinkilde	Valle
+Proteinkilde	Ærte
+Proteinkilde	Æg
+Proteinkilde	Hamp
+Proteinkilde	Ris
+Proteinkilde	Soja
+Proteinkilde	Chia
+Proteinkilde	Mandel
+Proteinkilde	Græskar
+Proteinkilde	Blandet
+Alder	0 mdr.
+Solfaktor	SPF 10
+Solfaktor	SPF 15
+Solfaktor	SPF 20
+Solfaktor	SPF 25
+Solfaktor	SPF 30
+Solfaktor	SPF 35
+Solfaktor	SPF 40
+Solfaktor	SPF 45
+Solfaktor	SPF 50
+	
+`;
+
+const rows = raw.split("\n");
+const data = [];
+rows.forEach((row) => {
+  const [key, value] = row.split("\t");
+  const index = data.findIndex((obj) => obj.key === key);
+  if (index > -1) {
+    data[index].values.push(value);
+  } else {
+    data.push({
+      key,
+      values: [value],
+    });
+  }
+});
+
+console.log(JSON.stringify(data));
