677e078b17c930a0202a13595d52f4846dec1a4e
kevkev14 <kevi5598@stud.kea.dk>
2023-11-21 20:49:24 +0100

    dd

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -7,31 +7,31 @@ function Popupbox() {
     currentQuestion.style.display = "none";
   }
 
-  function popupfunction() {}
-  return;
-  <>
-    <div class="popup">
-      <h2>Spørgeskema</h2>
-      <p class="question-text">Spørgsmål spørgsmåldawawd?</p>
-      <form id="questionForm">
-        <label for="option1">
-          <input type="checkbox" id="option1" value="value1" /> 1
-        </label>
-        <br />
-        <label for="option2">
-          <input type="checkbox" id="option2" value="value2" /> 2
-        </label>
-        <br />
-        <label for="option3">
-          <input type="checkbox" id="option3" value="value3" /> 2
-        </label>
-        <br />
-        <button type="button" onclick="nextQuestion()">
-          Næste
-        </button>
-      </form>
-    </div>
-  </>;
+  return (
+    <>
+      <div className={styles.Popup}>
+        <h2>Spørgeskema</h2>
+        <p className="question-text">Spørgsmål spørgsmåldawawd?</p>
+        <form id="questionForm">
+          <label htmlFor="option1">
+            <input type="checkbox" id="option1" value="value1" /> 1
+          </label>
+          <br />
+          <label htmlFor="option2">
+            <input type="checkbox" id="option2" value="value2" /> 2
+          </label>
+          <br />
+          <label htmlFor="option3">
+            <input type="checkbox" id="option3" value="value3" /> 2
+          </label>
+          <br />
+          <button type="button" onClick={nextQuestion()}>
+            Næste
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Popupbox;
