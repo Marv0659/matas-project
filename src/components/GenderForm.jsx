333287dab86b7d22fb0fe6255cae030dd02b488b
kevkev14 <kevi5598@stud.kea.dk>
2023-11-22 14:44:54 +0100

    testcommit

--- src/components/GenderForm.jsx
+++ src/components/GenderForm.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./style.css";
 
 function GenderForm() {
   return (
@@ -7,16 +8,49 @@ function GenderForm() {
         Mand
         <input id="mand" type="checkbox" />
       </label>
+
       <label htmlFor="kvinde">
         {" "}
         Kvinde
         <input id="kvinde" type="checkbox" />
       </label>
+
       <label htmlFor="underordnet">
         {" "}
         Underordnet
         <input id="underordnet" type="checkbox" />
       </label>
+
+      <div className="grid-container">
+        <div className="card">
+          <img src="./public/imgs/egenskab/glow.webp" alt="" className="card-image" />
+          <h3 className="cardtitle">Mand</h3>
+        </div>
+
+        <div className="card">
+          <img src="./public/imgs/egenskab/glow.webp" alt="" className="card-image" />
+          <h3 className="cardtitle">Kvinde</h3>
+        </div>
+
+        <div className="card">
+          <img src="./public/imgs/egenskab/glow.webp" alt="" className="card-image" />
+          <h3 className="cardtitle">Underordnet</h3>
+        </div>
+      </div>
+
+      {/* <label htmlFor="kvinde" className="card">
+        {" "}
+        Kvinde
+        <input id="kvinde" type="checkbox" />
+        <img src="./public/imgs/egenskab/glow.webp" alt="" className="card-image" />
+        <h3 className="cardtitle">Kvinde</h3>
+      </label> */}
+
+      {/* <label htmlFor="glowCheckbox" className="card">
+        <input id="glowCheckbox" type="checkbox" />
+        <img src="./public/imgs/egenskab/glow.webp" alt="" className="card-image" />
+        <h3 className="cardtitle">Glow</h3>
+      </label> */}
     </div>
   );
 }
--- src/components/GenderStepContent.jsx
+++ src/components/GenderStepContent.jsx
@@ -1,11 +1,14 @@
 import GenderForm from "./GenderForm";
+import "./titles.css";
 
 export default function GenderStepContent() {
   return (
     <div>
-      <h1>Hvilket køn er modtager?</h1>
-      <p>Vælg blot et kriterie</p>
-      <GenderForm />
+      <div className="genderbox">
+        <h1>Hvilket køn er modtager?</h1>
+        <p>Vælg blot et kriterie</p>
+        <GenderForm />
+      </div>
     </div>
   );
 }
