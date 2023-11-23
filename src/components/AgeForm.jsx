58304edc3cca631e629347fae563d9989f29e15f
Mill9510 <mill9510@stud.kea.dk>
2023-11-23 19:51:53 +0100

    style

--- src/components/AgeForm.jsx
+++ src/components/AgeForm.jsx
@@ -4,11 +4,11 @@ import CardTest from "./CardTest";
 function AgeForm() {
   return (
     <div className="grid-container">
-      <CardTest img="/imgs/alder/teen.webp" text="Teen" />
-      <CardTest img="/imgs/alder/20.webp" text="20erne" />
-      <CardTest img="/imgs/alder/30.webp" text="30erne" />
-      <CardTest img="/imgs/alder/40.webp" text="40erne" />
-      <CardTest img="/imgs/alder/50.webp" text="50erne" />
+      <CardTest img="/imgs/alder/teen.webp" text="Teenager" />
+      <CardTest img="/imgs/alder/20.webp" text="20'erne" />
+      <CardTest img="/imgs/alder/30.webp" text="30'erne" />
+      <CardTest img="/imgs/alder/40.webp" text="40'erne" />
+      <CardTest img="/imgs/alder/50.webp" text="50'erne" />
       <CardTest img="/imgs/alder/60.webp" text="60+" />
     </div>
   );
